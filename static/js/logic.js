b6f0553e8c7db5435cb45565d9730615c003a1e0
cindyford361 <cindyford361@gmail.com>
2020-07-11 14:46:58 -0400

    JS anime update

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,49 @@
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml5 .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 900
+  }).add({
+    targets: '.ml5 .line',
+    duration: 800,
+    easing: "easeOutExpo",
+    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+  }).add({
+    targets: '.ml5 .ampersand',
+    opacity: [0,1],
+    scaleY: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 800,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5 .letters-left',
+    opacity: [0,1],
+    translateX: ["0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 800,
+    offset: '-=300'
+  }).add({
+    targets: '.ml5 .letters-right',
+    opacity: [0,1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 800,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5',
+    opacity: 0,
+    duration: 1200,
+    easing: "easeOutExpo",
+    delay: 1600
+  });
+
+
+
+
+
+
 var coords = [48.7,-123.04121052819823];
 //var url = "http://hotline.whalemuseum.org/api.json?species=orca&near=48.5159,-123.1524&radius=100&limit=1000"
 //var base_url = "http://hotline.whalemuseum.org/api.json?&limit=1000"
